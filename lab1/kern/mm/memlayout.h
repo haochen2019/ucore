25f015ce02b950646df066fbd453ffd787b04a8a
haochen2019 <dhaochen@163.com>
2021-03-05 09:24:56 +0800

    lab 1 complete(includes  challenges)

--- lab1/kern/mm/memlayout.h
+++ lab1/kern/mm/memlayout.h
@@ -10,7 +10,7 @@
 #define SEG_UDATA    4
 #define SEG_TSS        5
 
-/* global descriptor numbers */
+/* global descriptor numbers 段选择子，由段号左移三位得到*/
 #define GD_KTEXT    ((SEG_KTEXT) << 3)        // kernel text
 #define GD_KDATA    ((SEG_KDATA) << 3)        // kernel data
 #define GD_UTEXT    ((SEG_UTEXT) << 3)        // user text
